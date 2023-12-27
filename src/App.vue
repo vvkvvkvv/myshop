c17cf5a82de6a8f58e258d65142af5776a0e3d29
vvkvvkvv <152476534+vvkvvkvv@users.noreply.github.com>
2023-12-27 19:30:06 +0000

    from base to template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <b-navbar toggleable="sm" type="light" variant="light">
+
+    <div class="container">
+
+        <b-navbar-brand href="#">Магазин</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Ссылка</b-nav-item>
+            <b-nav-item href="#" disabled>Отключено</b-nav-item>
+          </b-navbar-nav>
+
+        </b-collapse>
+
+    </div>
+
+  </b-navbar>
+
+    <div class="container">
+      <b-row>
+        <b-col cols="2">
+
+        <div class="mt-2">Поиск</div>
+        <b-form-input v-model="text"></b-form-input>
+        <br>
+        <div class="mt-2">Категории</div>
+        <b-list-group>
+          <b-list-group-item
+            @click="selectCategory(category)"
+            v-for="(category,id) in categories"
+            :key="id"
+            :active="category==currentCat"
+          >{{category}}</b-list-group-item>
+          <b-list-group-item
+            @click="selectCategory('')"
+            :active="currentCat==''"
+          >Все категории</b-list-group-item>
+        </b-list-group>
+        
+        </b-col>
+        <b-col cols="10">
+          <b-jumbotron header="BootstrapVue" lead="Компоненты Bootstrap v4 для Vue.js 2">
+            <p>Для получения дополнительной информации посетите веб-сайт</p>
+            <b-button variant="primary" href="#">Больше информации</b-button>
+          </b-jumbotron>
+          <hr>
+          <b-row>
+            <b-col cols="3" v-for="(good,id) in searchInGoods()" :key="id">
+
+              <b-card
+                
+                :title="good.title"
+                :img-src="good.image"
+                
+                img-alt="Изображение"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+              >
+                <b-card-text>
+                  {{ good.description }}
+                </b-card-text>
+                <h3>{{good.price}}</h3>
+                <b-button href="#" variant="primary">Купить</b-button> 
+              </b-card>
+            </b-col>
+          </b-row>
+
+
+        </b-col>
+      </b-row>
+    
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    goods: [],
+    categories: [],
+    currentCat: '',
+    text: ''
+  }),
+  methods: {
+    getAllGoods() {
+      fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+              this.goods = json
+            })
+    },
+    getAllCategories() {
+      fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=>{
+              this.categories = json
+            })
+    },
+    searchInGoods() {
+      var goods = []
+      if(this.currentCat == '') goods = this.goods
+      else goods = this.goods.filter(item => (item.category == this.currentCat))
+
+      if (this.text == '') return goods
+      return goods.filter((item) => {
+        return item.title.indexOf(this.text) >= 0
+      })
+    },
+    selectCategory(item){
+      this.currentCat = item
+    }
+  },
+  created() {
+    this.getAllGoods()
+    this.getAllCategories()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Импортируйте файлы CSS Bootstrap и BootstrapVue (порядок важен)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Сделайте BootstrapVue доступным для всего проекта
+Vue.use(BootstrapVue)
+// При желании установите плагин компонентов иконок BootstrapVue
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
